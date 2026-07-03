f0dc0b651d025bb45af76c6b0fd46ee6c1c3c5ea
Paridhilamichhane12 <paridhilamichhanee245@gmail.com>
2026-07-03 04:11:37 +0000

    thread

---
+++ threadsynchronization.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int counter = 0;
+pthread_mutex_t lock;
+
+void *increment(void *arg)
+{
+    pthread_mutex_lock(&lock);
+
+    counter++;
+    printf("Counter = %d\n", counter);
+
+    pthread_mutex_unlock(&lock);
+
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    pthread_t t1, t2, t3;
+
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_create(&t1, NULL, increment, NULL);
+    pthread_create(&t2, NULL, increment, NULL);
+    pthread_create(&t3, NULL, increment, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
+
+    pthread_mutex_destroy(&lock);
+
+    return 0;
+}
